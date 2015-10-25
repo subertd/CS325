b3f3cdf24a234d0fa4cfe5afbef1e63fb19fd279
Lucia Blackwell <lblackwe@udel.edu>
2015-10-25 15:03:40 -0700

    Added printing for changeslow and changegreedy to app.py

--- Project2/app.py
+++ Project2/app.py
@@ -1,6 +1,8 @@
 from console_reader.console_reader import ConsoleReader
 from file_reader.file_reader import FileReader
 from file_writer.file_writer import FileWriter
+from algorithm1.algorithm1 import changeslow
+from algorithm2.algorithm2 import changegreedy
 from algorithm3.algorithm3 import changedp
 
 consoleReader = ConsoleReader()
@@ -14,7 +16,17 @@ fileWriter = FileWriter(file_name + "change.txt")
 
 for i in data:
     V, A = i
+
+    result = changeslow(V, A)
+    fileWriter.write("changeslow:\n")
+    fileWriter.write_result(result)
+
+    result = changegreedy(V, A)
+    fileWriter.write("changegreedy:\n")
+    fileWriter.write_result(result)
+
     result = changedp(V, A)
+    fileWriter.write("changedp:\n")
     fileWriter.write_result(result)
 
 fileWriter.close()
