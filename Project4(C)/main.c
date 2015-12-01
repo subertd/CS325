a4785aa1af5386c2b9a1d7bbeaa5a11ee5ef14e6
Donald <subertd@onid.oregonstate.edu>
2015-12-01 12:10:34 -0800

    Implemented file reading

--- Project4(C)/main.c
+++ Project4(C)/main.c
@@ -1,9 +1,9 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_INPUT_LENGTH 256
-#define MAX_PROBLEM_SIZE 16000
+#define MAX_INPUT_LENGTH 16000
 
 struct problem {
   int id[MAX_INPUT_LENGTH];
@@ -24,26 +24,26 @@ int main(int argc, char** argv) {
 
 void get_file_input(char *input_file_name, struct problem *p) {
 
-  FILE *ifp;
-  char line[MAX_INPUT_LENGTH];
-
-  while ((c = getc(ifp)) != EOF) {
-
-  }
-
-
-  ifp = fopen(input_file_name, "r");
-  if (ifp == NULL) {
-    fprintf(stderr, "File not found\n");
+  FILE *fp;
+  char *line = NULL;
+  
+  fp = fopen(input_file_name, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "File not found%s\n", input_file_name);
     exit(EXIT_FAILURE);
   }
 
-  while(fgets(line, sizeof line, ifp) != NULL) {
-    printf("%s", line);
-
-
-
+  int id, x, y, i = 0;
+  while(fscanf(fp, "%d %d %d\n", &id, &x, &y) != EOF) {
+    // printf("Vertex %d, is at (%d, %d)\n", id, x, y);
+    p->id[i] = id;
+    p->x[i] = x;
+    p->y[i] = y;
+    ++i;
   }
+  p->size = i;
 
-  fclose(ifp);
+  fclose(fp);
+  if (line)
+    free(line);
 }
